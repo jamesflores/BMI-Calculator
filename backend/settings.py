59a64c2d0f54f508d8a39975e0f654eb4a60df50
James Flores <james@jamesflores.net>
2024-03-20 07:56:11 +1100

    move Django secret key to env var

--- backend/settings.py
+++ backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_vt9q#r#tyo17-37+hwa_0uju=z2au^$t20i1jlsv1n-p@vf#e'
+SECRET_KEY = os.environ.get('BMI_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
